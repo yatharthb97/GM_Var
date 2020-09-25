b875cfe4f02d724d5ff5d6d3c36e60b15b49bce2
Yatharth <yatharth1997@gmail.com>
2020-09-25 19:25:55 +0530

    Added some macros

--- macros.h
+++ macros.h
@@ -15,3 +15,29 @@
 
 //Define toinclude a Var v Var Relation plot.
 #define __VARVFRACVAR__
+
+
+//Leave the first __L_F_IT__ before shuffling the con_matrix order
+#define __L_F_IT__ 1
+
+
+//Machine Epsilon for double comparision
+#define __M_EPSILON__ 0.00000001
+//### Machine Epsilon  - 10e-8
+
+
+
+
+
+
+
+
+
+
+
+// These codes set the actual text to the specified color
+#define RESETTEXT  "\x1B[0m" // Set all colors back to normal.
+#define FORERED  "\x1B[31m" // Red
+#define FOREBLU  "\x1B[34m" // Blue
+#define FORECYN  "\x1B[36m" // Cyan
+#define FOREWHT  "\x1B[37m" // White
